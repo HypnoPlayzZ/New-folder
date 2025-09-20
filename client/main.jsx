6949307c0394c8e3f936a840c2f9217b564aaad3
HypnoPlayzZ <chtnbahuguna1@gmail.com>
2025-09-20 16:39:47 +0530

    jh

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App.jsx'
+import App from './App.jsx'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-  
 )
